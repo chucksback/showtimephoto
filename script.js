dee34f3a2dc53d2e9d042af4dfbabb15dff975dc
chuckster <xdviper@gmail.com>
2024-11-13 08:28:18 -0500

    initial

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for button interaction
+document.querySelector('.book-now-button').addEventListener('click', (event) => {
+    event.preventDefault(); // Prevents the link from navigating away
+    alert('Booking functionality coming soon!');
+});
